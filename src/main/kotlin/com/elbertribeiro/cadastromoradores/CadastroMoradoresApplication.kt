c10b2f8d6aeeb94c45c6936915f34a2908ef82cc
elbertribeiro <elbertprofissional@gmail.com>
2023-01-17 11:10:23 -0300

    first commit

---
+++ src/main/kotlin/com/elbertribeiro/cadastromoradores/CadastroMoradoresApplication.kt
@@ -0,0 +1,11 @@
+package com.elbertribeiro.cadastromoradores
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CadastroMoradoresApplication
+
+fun main(args: Array<String>) {
+	runApplication<CadastroMoradoresApplication>(*args)
+}
