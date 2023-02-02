f704fd4f9590f40d771f3a6103abddc5b1cda7a4
elbertribeiro <elbertprofissional@gmail.com>
2023-02-02 18:59:33 -0300

    feat: add testes unitarios

--- cadastro-moradores-api/src/test/java/com/elbertribeiro/cadastromoradores/usuario/UsuarioServicoTest.java
+++ cadastro-moradores-api/src/test/java/com/elbertribeiro/cadastromoradores/usuario/UsuarioServicoTest.java
@@ -1,9 +1,32 @@
 package com.elbertribeiro.cadastromoradores.usuario;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
-public class UsuarioServicoTest {
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class UsuarioServicoTest {
+
+    @Mock
+    List<Usuario> usuarios;
+    Usuario usuario = mock(Usuario.class);
+    UsuarioRepositorio repositorio = mock(UsuarioRepositorio.class);
+
     @Test
-    void buscarListaUsuarios() {
+    void buscarListaUsuariosTest() {
+        when(repositorio.findAll()).thenReturn(usuarios);
+        UsuarioServico usuarioServico = new UsuarioServico(repositorio);
+        Assertions.assertEquals(usuarios, usuarioServico.buscarListaUsuarios());
+    }
+
+    @Test
+    void salvarUsuarioTest() {
+        when(repositorio.save(usuario)).thenReturn(usuario);
+        UsuarioServico usuarioServico = new UsuarioServico(repositorio);
+        Assertions.assertNotNull(usuarioServico.salvarUsuario(usuario));
     }
 }
