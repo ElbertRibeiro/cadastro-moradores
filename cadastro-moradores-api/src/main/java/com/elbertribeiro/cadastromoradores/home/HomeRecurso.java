028d35f19e991ac91630e27d0f7d09c5df7d4f65
elbertribeiro <elbertprofissional@gmail.com>
2023-02-02 16:45:17 -0300

    feat: add swagger

--- cadastro-moradores-api/src/main/java/com/elbertribeiro/cadastromoradores/home/HomeRecurso.java
+++ cadastro-moradores-api/src/main/java/com/elbertribeiro/cadastromoradores/home/HomeRecurso.java
@@ -1,6 +1,7 @@
 package com.elbertribeiro.cadastromoradores.home;
 
 import com.elbertribeiro.cadastromoradores.usuario.UsuarioRecurso;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RestController
 @RequestMapping("/")
 public class HomeRecurso {
+    @Operation(summary = "Listar todas as collections")
     @GetMapping
     public ResponseEntity<HomeDto> getEntryPoint() {
         HomeDto rootEntryPoint = new HomeDto()
--- cadastro-moradores-api/src/main/java/com/elbertribeiro/cadastromoradores/usuario/UsuarioRecurso.java
+++ cadastro-moradores-api/src/main/java/com/elbertribeiro/cadastromoradores/usuario/UsuarioRecurso.java
@@ -1,6 +1,8 @@
 package com.elbertribeiro.cadastromoradores.usuario;
 
 import com.elbertribeiro.cadastromoradores.message.MessageResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,13 +18,17 @@ public class UsuarioRecurso {
         this.usuarioServico = usuarioServico;
     }
 
+    @Operation(summary = "Listar todos usuarios")
+    @ApiResponse(responseCode = "200", description = "Usuarios retornados com sucesso")
     @GetMapping
     public ResponseEntity<List<Usuario>> listaUsuarios() {
         return ResponseEntity.ok(usuarioServico.buscarListaUsuarios());
     }
 
+    @Operation(summary = "Salvar usuario")
+    @ApiResponse(responseCode = "201", description = "Retorna ID do usuario criado")
     @PostMapping
-    public ResponseEntity<MessageResponse> salvaUsuario(@RequestBody Usuario usuario){
+    public ResponseEntity<MessageResponse> salvaUsuario(@RequestBody Usuario usuario) {
         return ResponseEntity.status(HttpStatus.CREATED).body(usuarioServico.salvarUsuario(usuario));
     }
 }
